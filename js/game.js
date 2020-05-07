8c1a7a747dce6a78156bfab1845cf5a121e48af3
Nicholas DeLello <nicholas_delello@yahoo.com>
2020-05-07 13:48:53 -0400

    Implement cries, make them toggleable.

--- js/game.js
+++ js/game.js
@@ -6,19 +6,27 @@ var spaces = {
     pikachu: 4,
     beedrill: 5,
     pewterGym: 6,
+    geodude: 6,
+    onix: 6,
     nidoran: 7,
     zubat: 8,
     clefairy: 9,
     jigglypuff: 10,
     abra1: 11,
     gary1: 12,
+    squirtle: 12,
+    charmander: 12,
+    bulbasaur: 12,
     ceruleanGym: 13,
+    staryu: 13,
+    starmie: 13,
     slowpoke: 14,
     bellsprout: 15,
     meowth: 16,
     diglett: 17,
     sSAnne: 18,
     vermillionGym: 19,
+    raichu: 19,
     bicycle: 20,
     magikarp: 21,
     sandshrew: 22,
@@ -28,21 +36,33 @@ var spaces = {
     cubone: 26,
     silphScope: 27,
     abra2: 28,
-    snowlax: 29,
+    snorlax: 29,
     gary2: 30,
+    ivysaur: 30,
+    charmeleon: 30,
+    wartortle: 30,
     eevee: 31,
+    jolteon: 31,
+    vaporeon: 31,
+    flareon: 31,
     caledonGym: 32,
+    weepinbell: 32,
+    gloom: 32,
+    tangela: 32,
     psyduck: 33,
     evolution: 34,
     porygon: 35,
     silphCo: 36,
     scientist: 37,
+    manemite: 37,
+    magneton: 37,
     lapras: 38,
     teamRocket: 39,
     giovanni: 40,
     rareCandy: 41,
     gary3: 42,
     saffronGym: 43,
+    alakazam: 43,
     hitmon: 44,
     hitmonlee: 44,
     hitmonchamp: 44,
@@ -51,14 +71,16 @@ var spaces = {
     doduo: 47,
     safariZone: 48,
     dratini: 49,
-    taurus: 50,
+    tauros: 50,
     chansey: 51,
     fuchsiaGym: 52,
+    venonat: 52,
+    venomoth: 52,
     electrode: 53,
     electabuzz: 54,
     poliwag: 55,
     seaking: 56,
-    missigno: 57,
+    missingno: 57,
     cinnabarGym: 58,
     koffing: 59,
     fossilPokemon: 60,
@@ -70,27 +92,199 @@ var spaces = {
     pokeball: 61,
     persian: 62,
     viridianGym: 63,
+    nidoqueen: 63,
+    nidoking: 63,
     fearow: 64,
     graveler: 65,
     gyrados: 66,
     dragonite: 67,
     articuno: 68,
     zapdos: 68,
-    moltres: 69,
-    eliteFour: 70,
-    gary4: 71,
-    pokemonMaster: 72
+    moltres: 68,
+    eliteFour: 69,
+    gary4: 70,
+    blastoise: 70,
+    charizard: 70,
+    venusaur: 70,
+    mewtwo: 71,
+    pokemonMaster: 71
 }
-var reverseSpaces = {}
-for (let k in spaces)
-    if (spaces[k] in reverseSpaces)
-        if (!Array.isArray(reverseSpaces[spaces[k]]))
-            reverseSpaces[spaces[k]] = [reverseSpaces[spaces[k]], k]
-        else {
-            reverseSpaces[spaces[k]].push(k)
+
+let pokemonIds = {
+    missingno: 0,
+    bulbasaur: 1,
+    ivysaur: 2,
+    venusaur: 3,
+    charmander: 4,
+    charmeleon: 5,
+    charizard: 6,
+    squirtle: 7,
+    wartortle: 8,
+    blastoise: 9,
+    caterpie: 10,
+    metapod: 11,
+    butterfree: 12,
+    weedle: 13,
+    kakuna: 14,
+    beedrill: 15,
+    pidgey: 16,
+    pidgeotto: 17,
+    pidgeot: 18,
+    rattata: 19,
+    raticate: 20,
+    spearow: 21,
+    fearow: 22,
+    ekans: 23,
+    arbok: 24,
+    pikachu: 25,
+    raichu: 26,
+    sandshrew: 27,
+    sandslash: 28,
+    nidoranM: 29,
+    nidorina: 30,
+    nidoqueen: 31,
+    nidoranF: 32,
+    nidorino: 33,
+    nidoking: 34,
+    clefairy: 35,
+    clefable: 36,
+    vulpix: 37,
+    ninetales: 38,
+    jigglypuff: 39,
+    wigglytuff: 40,
+    zubat: 41,
+    golbat: 42,
+    oddish: 43,
+    gloom: 44,
+    vileplume: 45,
+    paras: 46,
+    parasect: 47,
+    venonat: 48,
+    venomoth: 49,
+    diglett: 50,
+    dugtrio: 51,
+    meowth: 52,
+    persian: 53,
+    psyduck: 54,
+    golduck: 55,
+    mankey: 56,
+    primeape: 57,
+    growlithe: 58,
+    arcanine: 59,
+    poliwag: 60,
+    poliwhirl: 61,
+    poliwrath: 62,
+    abra: 63,
+    kadabra: 64,
+    alakazam: 65,
+    machop: 66,
+    machoke: 67,
+    machamp: 68,
+    bellsprout: 69,
+    weepinbell: 70,
+    victreebel: 71,
+    tentacool: 72,
+    tentacruel: 73,
+    geodude: 74,
+    graveler: 75,
+    golem: 76,
+    ponyta: 77,
+    rapidash: 78,
+    slowpoke: 79,
+    slowbro: 80,
+    magnemite: 81,
+    magneton: 82,
+    farfetch: 83,
+    doduo: 84,
+    dodrio: 85,
+    seel: 86,
+    dewgong: 87,
+    grimer: 88,
+    muk: 89,
+    shellder: 90,
+    cloyster: 91,
+    gastly: 92,
+    haunter: 93,
+    gengar: 94,
+    onix: 95,
+    drowzee: 96,
+    hypno: 97,
+    krabby: 98,
+    kingler: 99,
+    voltorb: 100,
+    electrode: 101,
+    exeggcute: 102,
+    exeggutor: 103,
+    cubone: 104,
+    marowak: 105,
+    hitmonlee: 106,
+    hitmonchan: 107,
+    lickitung: 108,
+    koffing: 109,
+    weezing: 110,
+    rhyhorn: 111,
+    rhydon: 112,
+    chansey: 113,
+    tangela: 114,
+    kangaskhan: 115,
+    horsea: 116,
+    seadra: 117,
+    goldeen: 118,
+    seaking: 119,
+    staryu: 120,
+    starmie: 121,
+    mr: 122,
+    scyther: 123,
+    jynx: 124,
+    electabuzz: 125,
+    magmar: 126,
+    pinsir: 127,
+    tauros: 128,
+    magikarp: 129,
+    gyarados: 130,
+    lapras: 131,
+    ditto: 132,
+    eevee: 133,
+    vaporeon: 134,
+    jolteon: 135,
+    flareon: 136,
+    porygon: 137,
+    omanyte: 138,
+    omastar: 139,
+    kabuto: 140,
+    kabutops: 141,
+    aerodactyl: 142,
+    snorlax: 143,
+    articuno: 144,
+    zapdos: 145,
+    moltres: 146,
+    dratini: 147,
+    dragonair: 148,
+    dragonite: 149,
+    mewtwo: 150,
+    mew: 151
+}
+
+
+var spaceToId = {}
+for (let k in spaces) {
+    if (k in pokemonIds) {
+        if (spaces[k] in spaceToId) {
+            if (!Array.isArray(spaceToId[spaces[k]])) {
+                spaceToId[spaces[k]] = [spaceToId[spaces[k]], pokemonIds[k]]
+            } else {
+                spaceToId[spaces[k]].push(pokemonIds[k])
+            }
+        } else {
+            spaceToId[spaces[k]] = pokemonIds[k]
         }
-    else
-        reverseSpaces[spaces[k]] = k
+    }
+}
+spaceToId[spaces.nidoran] = [pokemonIds.nidoranM, pokemonIds.nidoranF]
+spaceToId[spaces.missingno] = pokemonIds.nidoranF // This isn't _quite_ right, but it's close enough.
+spaceToId[spaces.abra1] = pokemonIds.abra
+spaceToId[spaces.abra2] = pokemonIds.abra
+
 
 var hard = {
     "stop": [
@@ -205,14 +399,14 @@ function onPlay(event) {
                 let tagLabel = document.getElementById("tags").appendChild(document.createElement("div"))
                 tagLabel.textContent = currentPlayer + ":"
                 let tags = json.players[count].tags
-                json.players[count].tags.add = function(val) {
+                json.players[count].tags.add = function (val) {
                     if (!tags.has(val)) {
                         let tag = tagLabel.appendChild(document.createElement("div"))
                         tag.textContent = val
                         tags.__proto__.add.call(tags, val)
                     }
                 }
-                json.players[count].tags.delete = function(val) {
+                json.players[count].tags.delete = function (val) {
                     if (tags.has(val)) {
                         for (let children = tagLabel.childElementCount - 1; children >= 0; children--)
                             if (tagLabel.children[children].textContent === val) {
@@ -270,7 +464,38 @@ function roll(offsetTime) {
     echo("");
 }
 
+let cry = new Audio('https://pokemoncries.com/cries-old/1.mp3')
+let criesCheckbox = document.getElementById("cries")
+
 function checkSpecial(num) {
+    // Play cries
+    if (cries.checked) {
+        let space = json.players[json.selectedPlayer].space
+        if (space === spaces.gary3) // Workaround since both spaces have the same exact pokemon on them
+            space = spaces.gary2
+        let pokemonOnSpace = spaceToId[space]
+        if (typeof pokemonOnSpace === "number") {
+            cry.src = 'https://pokemoncries.com/cries-old/' + pokemonOnSpace + '.mp3'
+            cry.oncanplaythrough = function () {
+                cry.play()
+            }
+        } else if (pokemonOnSpace !== undefined) {
+            let playCurrent = function (index) {
+                if (index < pokemonOnSpace.length) {
+                    console.log("Playing cry for " + pokemonOnSpace)
+                    cry.src = 'https://pokemoncries.com/cries-old/' + pokemonOnSpace[index] + '.mp3'
+                    cry.oncanplaythrough = function () {
+                        cry.play()
+                        cry.onended = function () {
+                            playCurrent(index + 1)
+                        }
+                    }
+                }
+            }
+            playCurrent(0)
+        }
+    }
+
     let currentPlayer = json.players[num]
     let modal = document.getElementById("modal")
     let removeAll = function () {
@@ -320,7 +545,7 @@ function checkSpecial(num) {
             currentPlayer.tags.add("Karpal Tunnel")
             break;
         case spaces.sandshrew:
-            currentPlayer.tags.add("Non-dominant only")
+            currentPlayer.tags.add("Non-dominant")
             break;
         case spaces.zubat:
             currentPlayer.tags.add("Batty")
@@ -433,7 +658,7 @@ function checkSpecial(num) {
                     }
                     let loseBtn = modal.appendChild(document.createElement("button"))
                     loseBtn.textContent = chosenPlayer
-                    loseBtn.onclick = function() {
+                    loseBtn.onclick = function () {
                         for (let player = 0; player < json.players.length; player++) {
                             if (json.players[player].name !== chosenPlayer) {
                                 if (player === json.selectedPlayer)
@@ -450,35 +675,35 @@ function checkSpecial(num) {
         case spaces.electabuzz:
             currentPlayer.lostTurns++
             break;
-        case spaces.missigno:
+        case spaces.missingno:
             currentPlayer.tags.add("Glitched")
             roll()
             setTimeout(function () {
                 let val = parseInt(document.getElementById('move').value, 10)
                 if (val >= 5)
                     currentPlayer.tags.delete("Glitched")
-            }, 600);
-            roll(600)
+            }, 3000);
+            roll(3000)
             setTimeout(function () {
                 let val = parseInt(document.getElementById('move').value, 10)
                 if (val >= 5)
                     currentPlayer.tags.delete("Glitched")
-            }, 1200);
-            roll(1200)
+            }, 6000);
+            roll(6000)
             setTimeout(function () {
                 let val = parseInt(document.getElementById('move').value, 10)
                 if (val >= 5)
                     currentPlayer.tags.delete("Glitched")
                 if (currentPlayer.tags.has("Glitched"))
                     go(spaces.palletTown)
-            }, 1800);
+            }, 9000);
             break;
         case spaces.dragonite:
             currentPlayer.lostTurns++
             break;
         case spaces.vermillionGym:
             roll();
-            setTimeout(function() {
+            setTimeout(function () {
                 let val = parseInt(document.getElementById('move').value, 10)
                 if (val % 2 === 0)
                     currentPlayer.lostTurns++
